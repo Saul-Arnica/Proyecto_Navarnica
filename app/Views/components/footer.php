a35b208b72d2c93b11af4ee963776af518f1a609
Sergio Navarro <nachonavarro197@gmail.com>
2025-04-15 22:08:41 -0300

    fijate pue si te anda

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,4 +1,7 @@
-    <footer class="text-center py-3">
-        <p class="text-center">Bienvenido a la Veterinaria Navarnica, donde cuidamos de tus mascotas como si fueran nuestras.</p>
-        <p class="text-center my-1">&copy;2025 Veterinaria Navarnica - Todos los derechos reservados</p>
-    </footer>
+        <footer class="text-center py-3">
+            <p class="text-center">Bienvenido a la Veterinaria Navarnica, donde cuidamos de tus mascotas como si fueran nuestras.</p>
+            <p class="text-center my-1">&copy;2025 Veterinaria Navarnica - Todos los derechos reservados</p>
+        </footer>
+        <script src="../assets/js/bootstrap.bundle.js"></script>
+    </body>
+</html>
--- app/Views/informacionContacto.php
+++ app/Views/informacionContacto.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VeteNavarnica</title>
-</head>
-<body>
-    <header>
-        <h1>Info Contact</h1>
-    </header>
-
-    <main>
-    
-    </main>
-
-    <footer>
-        <p>&copy; 2025 Mi Página Web</p>
-    </footer>
-</body>
-</html>
+<?php include_once("../Views/components/navbar.php"); ?>
+    <section>
+        <h1>Información de Contacto</h1>
+    </section>
+<?php include_once("../Views/components/footer.php"); ?>
