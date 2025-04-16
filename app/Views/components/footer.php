3e8a341ac7cb2b557eacdc885bafc0a4d73babf7
Saul-Arnica <saulagustinarnica@outlook.com>
2025-04-15 21:56:25 -0300

    #components

---
+++ app/Views/components/footer.php
@@ -0,0 +1,4 @@
+    <footer class="text-center py-3">
+        <p class="text-center">Bienvenido a la Veterinaria Navarnica, donde cuidamos de tus mascotas como si fueran nuestras.</p>
+        <p class="text-center my-1">&copy;2025 Veterinaria Navarnica - Todos los derechos reservados</p>
+    </footer>
--- app/Views/principal.php
+++ app/Views/principal.php
@@ -1,112 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8"> <!-- Codificacion de caracteres -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Para que la pagina se vea bien en dispositivos moviles -->
-    <meta name="description" content="Veterinaria Navarnica - La mejor atención de Corrientes Capital"> <!-- Descripción de la página cuando la buscamos-->
-    <title>VeteNavarnica</title> <!-- Titulo de la pestaña -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.css"/> <!-- Bootstrap CSS -->
-    <script src="../assets/js/bootstrap.bundle.js"></script> <!-- Bootstrap JS -->
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png" /> <!-- Icono de la pestaña -->
-    <link rel="canonical" href="http://VeterinariaNavarnica.com/" /> <!-- URL canónica para evitar contenido duplicado y que tome -->
-    <link rel="stylesheet" href="../assets/css/styles.css"/> <!-- CSS personalizado -->
+    
 </head> 
-<body class="background-page> <!-- Cuerpo de la pagina -->
-    <header class="text-center">
-        <img src="../assets/img/logoVete.png" alt="Logo Veterinaria" class="img-fluid logo-vete"/> <!-- Logo de la veterinaria utilizado un CSS personalizado -->
-    </header>
-    <div class="text-center">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/">Inicio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Consultas
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/quienesSomos">¿Quienes Somos?</a></li>
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/informacionContacto">Contactos</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/terminosYUsos">Terminos y Usos</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Tienda
-                        </a>
-                        <ul class="dropdown-menu">  
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/catalogoProductos">Catalogo de Productos</a></li>
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/comercializacion">Comercializacion</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="http://localhost/proyecto_Arnica_Saul_Agustin/public/informacionContacto">Informacion de Contacto</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search" value=" ">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+<body class="background-page"> <!-- Cuerpo de la pagina -->
+    
 
     <main class="flex-fill">
     <!-- Aquí va tu contenido -->
-    
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
     </main>
     <aside>
         <p>La mejor atención de Corrientes Capital</p>
     </aside>
-    <footer class="text-center py-3">
-    <p class="text-center">Bienvenido a la Veterinaria Navarnica, donde cuidamos de tus mascotas como si fueran nuestras.</p>
-    <p class="text-center my-1">&copy;2025 Veterinaria Navarnica - Todos los derechos reservados</p>
-    </footer>
+    
 </body>
 </html> 
