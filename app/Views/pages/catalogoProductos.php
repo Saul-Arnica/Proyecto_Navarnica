0becc82395ebe4d0c04d1aa948f50047fcbbf189
Saul-Arnica <saulagustinarnica@outlook.com>
2025-04-16 22:22:18 -0300

    #Moving folders

---
+++ app/Views/pages/catalogoProductos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VeteNavarnica</title>
+</head>
+<body>
+    <header>
+        <h1>Catalogo</h1>
+    </header>
+
+    <main>
+    
+    </main>
+
+    <footer>
+        <p>&copy; 2025 Mi Página Web</p>
+    </footer>
+</body>
+</html>
---
+++ app/Views/pages/informacionContacto.php
@@ -0,0 +1,5 @@
+<?php include_once("components/navbar.php"); ?>
+    <section>
+        <h1>Información de Contacto</h1>
+    </section>
+<?php include_once("components/footer.php"); ?>
