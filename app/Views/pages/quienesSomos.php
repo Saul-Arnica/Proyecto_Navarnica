a94233a200dc198bab8a79bf78e3d150c84a8843
Sergio Navarro <nachonavarro197@gmail.com>
2025-04-19 06:10:14 -0300

    prueba 876

--- app/Views/pages/quienesSomos.php
+++ app/Views/pages/quienesSomos.php
@@ -2,15 +2,23 @@
     <div class="mt-5 mb-5">
         <h2 class="text-center ">Quienes Somos</h2>
     </div>
-    
+
     <div class="row align-items-center mt-5 mb-5 p-3">
         <div class="col-12 col-lg-6 mb-3 mb-lg-0">
-        <img src="../public/assets/img/quienessomos.avif" class="rounded img-fluid" alt="...">
+            <img src="../public/assets/img/quienessomos.avif" class="rounded img-fluid" alt="...">
         </div>
         <div class="col-12 col-lg-6">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto architecto quibusdam vel repellat doloremque illo quaerat. Ad, maxime repudiand
-                ae? Laudantium, porro! Ipsum ullam culpa deleniti molestias, et est sapiente.
+            <p class="fs-6 fs-lg-5 fw-semibold">
+                En nuestra veterinaria, compartimos una pasión profunda por los animales y el compromiso de brindarles
+                la mejor atención posible. Somos un equipo de profesionales dedicados al bienestar de las mascotas, con
+                años de experiencia en medicina veterinaria, prevención, diagnóstico y tratamiento.
+
+                Creemos que cada animal merece ser tratado con amor, respeto y cuidado personalizado. Por eso,
+                trabajamos cada día para construir una relación cercana con nuestros pacientes de cuatro patas y sus
+                familias, ofreciendo un servicio integral que combina conocimiento, empatía y tecnología.
+
+                Nuestra misión es acompañarte en cada etapa de la vida de tu mascota, desde sus primeros pasos hasta su
+                vejez, con calidez humana y vocación de servicio.
             </p>
         </div>
     </div>
