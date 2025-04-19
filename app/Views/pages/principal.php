7998b5395cbcb8a2d34fa36278572cc93d8827d0
Saul-Arnica <saulagustinarnica@outlook.com>
2025-04-19 06:58:27 -0300

    Terminando(Momentaneamente) el principal y derivados.

--- app/Views/pages/principal.php
+++ app/Views/pages/principal.php
@@ -1,13 +1,24 @@
-<section class="container-fluid principal-custom">
-    <div class="container-fluid principal-img-bg text-center ">
+<section class="principal-custom">
+    <div class="container-fluid principal-img-bg text-center">
         <!-- Card: Solicitar Turno -->
-        <div class="col-12 col-md-6">
-            <div class="card h-100 shadow-sm">
-                <!--<img src="../public/assets/img/fondo.jpg" class="card-img-top" alt="Solicitar turno">-->
-                <div class="card-body text-center">
-                    <h5 class="card-title">Solicitar Turno</h5>
-                    <p class="card-text">Reservá una consulta para tu mascota de forma rápida y sencilla.</p>
-                    <a href="turnos.php" class="btn btn-info text-white">Solicitar Turno</a>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 col-lg-4 mb-3 mt-3">
+                <div class="card w-10 shadow-sm rounded-pill card-custom1 ">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Reserva tu turno</h5>
+                        <p class="card-text">Para tu mascota de forma rápida y sencilla.</p>
+                        <a href="turnos.php" class="btn btn-primary text-white rounded-pill">Solicitar Turno</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Card: Ver productos -->
+            <div class="col-12 col-md-6 col-lg-4 mb-3 mt-3">
+                <div class="card w-10 shadow-sm rounded-pill card-custom2 text-white">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Productos Caninos y Felinos!</h5>
+                        <p class="card-text">Los mejores precios de Corrientes Capital.</p>
+                        <a href="turnos.php" class="btn btn-info text-white rounded-pill color-">Ver Productos</a>
+                    </div>
                 </div>
             </div>
         </div>
