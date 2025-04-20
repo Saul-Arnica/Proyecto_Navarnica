4d9728d18a213a202d30d81cf97fd9db32b95290
Saul-Arnica <saulagustinarnica@outlook.com>
2025-04-20 02:38:24 -0300

    Iniciando comercializacion

--- app/Views/pages/comercializacion.php
+++ app/Views/pages/comercializacion.php
@@ -1,3 +1,54 @@
 <section>
-    <h1>COMERCIALIZACION</h1>
+
+    <div class="container-fluid mt-4 mb-3 text-center">
+        <h2>COMERCIALIZACION</h2>
+        <!-- Carrusel de Imagenes de Comercializacion-->
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active" style="max-height: 500px;">
+                    <img src="../public/assets/img/perritoygatito.jpg" class="d-block w-100" alt="...">
+                </div>
+                
+                <div class="carousel-item col-md-6" style="max-height: 500px;"> 
+                    <img src="../public/assets/img/Otrologo.png" class="d-block w-25" alt="...">
+                </div>
+                
+                <div class="carousel-item" style="max-height: 500px;">
+                    <img src="../public/assets/img/veterinario-gsc1.jpg" class="d-block w-100" alt="...">
+                </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!--Iconos de Comercializacion-->
+        <div class="row mt-2">
+
+            <div class="col-lg-4">
+                <img src="../public/assets/img/formadepago.png" class="logos-comercializacion" alt="...">
+                <p><strong>Aceptamos todo tipo de tarjetas y efectivo</strong></p>
+            </div>
+
+            <div class="col-lg-4">
+                <img src="../public/assets/img/envio.png" class="logos-comercializacion" alt="...">
+                <p><strong>Se envia a domicilio</strong></p>
+            </div>
+
+            <div class="col-lg-4">
+                <img src="../public/assets/img/tienda.png" class="logos-comercializacion" alt="...">
+                <p><strong>Se puede retirar por sucursal</strong></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </section>
