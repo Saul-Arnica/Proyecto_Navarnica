6eac206553f0638f49d6313d69b6326b7226864e
Saul-Arnica <saulagustinarnica@outlook.com>
2025-06-25 16:48:21 -0300

    Ante ultimos cambios antes de entregar

--- app/Views/pages/carrito.php
+++ app/Views/pages/carrito.php
@@ -1,6 +1,31 @@
 <section class="container p-4">
     <h2 class="text-center">Carrito de compras</h2>
+    <?php if (session()->get('error')): ?>
+                    <div class="alert alert-danger">
+                        <?= esc(session()->get('error')) ?>
+                    </div>
+                <?php endif; ?>
 
+                <?php if (session()->get('errors')): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="mb-0">
+                            <?php foreach (session()->get('errors') as $error): ?>
+                                <li><?= esc($error) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <?php if (session()->get('success')): ?>
+                    <div class="alert alert-success">
+                        <?= esc(session()->get('success')) ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if (session()->get('info')): ?>
+                    <div class="alert alert-info">
+                        <?= esc(session()->get('info')) ?>
+                    </div>
+                <?php endif; ?>
     <div class="container-fluid">
         <?php if (empty($carrito)): ?>
             <div>
--- app/Views/pages/gestion/categoriasGestion.php
+++ app/Views/pages/gestion/categoriasGestion.php
@@ -3,7 +3,32 @@
         <h2>Categorias</h2>
         <a href="<?= base_url('gestion/altaCategoria') ?>" class="btn boton-custom">+ Agregar categoria</a>
     </div>
+    <?php if (session()->get('error')): ?>
+                    <div class="alert alert-danger">
+                        <?= esc(session()->get('error')) ?>
+                    </div>
+                <?php endif; ?>
 
+                <?php if (session()->get('errors')): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="mb-0">
+                            <?php foreach (session()->get('errors') as $error): ?>
+                                <li><?= esc($error) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <?php if (session()->get('success')): ?>
+                    <div class="alert alert-success">
+                        <?= esc(session()->get('success')) ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if (session()->get('info')): ?>
+                    <div class="alert alert-info">
+                        <?= esc(session()->get('info')) ?>
+                    </div>
+                <?php endif; ?>
     <table class="table table-striped table-bordered">
         <thead>
             <tr>
